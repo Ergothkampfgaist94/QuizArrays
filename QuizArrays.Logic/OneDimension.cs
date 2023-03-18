dea8d483ddbb712f9eb037be97fa2040287c4372
salan404 <salan404@SALAN404-6>
2023-03-17 19:48:51 -0500

    Add project files.

---
+++ QuizArrays.Logic/OneDimension.cs
@@ -0,0 +1,31 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace QuizArrays.Logic
+{
+    public class OneDimension
+    {
+        private int[] _array;
+        private int _top;
+
+        private bool IsEmpty => _top == 0;
+        private bool IsFull => _top == N;
+        public int N { get; }
+        public void Add(int number)
+        {
+            if (IsFull)
+            {
+                throw new Exception("The array is full.");
+            }
+            _array[_top] = number;
+            _top++;
+        }
+        public void OneDimension(int N)
+        {
+            _array[_top] = N;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ QuizArrays.Logic/TwoDimensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizArrays.Logic
+{
+    public class TwoDimensions
+    {
+        private int[,] _array;
+        public int M { get;}
+        public int N { get;}
+
+        public int TwoDimensions(int M, int N) 
+        {
+            return _array[M, N];
+        }
+        public void Fill()
+        {
+            var random = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                _array[i,i] = random.Next(1, 100);
+            }                  
+        }
+        public int ToOneDimension() 
+        { 
+            
+        }
+    }
+}
